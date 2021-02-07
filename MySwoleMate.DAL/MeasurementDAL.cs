4837bab41c3406969bc8bff0823ffc27fd86b57f
azimibm <sambm2512@gmail.com>
2021-02-07 14:54:13 -0600

    The EntityFramework has been all set up ans scheduleDAL and MeasurementDAL are ready to use.

---
+++ MySwoleMate.DAL/MeasurementDAL.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySwoleMate.Models;
+
+namespace MySwoleMate.DAL
+{
+  class MeasurementDAL
+  {
+    public List<Measurement> getMeasurements()
+    {
+      List<Measurement> allMeasuremnets = new List<Measurement>();
+      using (MySwoleMateEntities db = new MySwoleMateEntities())
+      {
+        allMeasuremnets = db.Measurements.ToList();
+      }
+      return allMeasuremnets;
+    }
+
+    //Get Measurement By Id
+    public Measurement GetMeasurementById(int mId)
+    {
+      Measurement m = new Measurement();
+      using (MySwoleMateEntities db = new MySwoleMateEntities())
+      {
+        m = db.Measurements.Find();
+      }
+      return m;
+    }
+
+    //Edit Measurement
+    public int EditMeasurement(Measurement editMes)
+    {
+      using (MySwoleMateEntities db = new MySwoleMateEntities())
+      {
+        var m = new Measurement { MeasurementID = editMes.MeasurementID, Weight = editMes.Weight, Waist = editMes.Waist, 
+                                  BodyFat = editMes.BodyFat, Chest = editMes.Chest, UpperArm = editMes.UpperArm};
+        db.Entry(m).State = System.Data.Entity.EntityState.Modified;
+        return db.SaveChanges();
+      }
+    }
+
+    //Add a Measurement
+    public int AddMeasurement(Measurement newMes)
+    {
+      Measurement mes = new Measurement()
+      {
+        Weight = newMes.Weight,
+        Waist = newMes.Waist,
+        BodyFat = newMes.BodyFat,
+        Chest = newMes.Chest,
+        UpperArm = newMes.UpperArm,
+        ScheduleID = newMes.ScheduleID
+      };
+
+      using (MySwoleMateEntities db = new MySwoleMateEntities())
+      {
+        db.Measurements.Add(mes);
+        return db.SaveChanges();
+      }
+    }
+
+    //Delete a Measurement
+    public void DeleteMeasurement(int mesId)
+    {
+      Measurement deleteMes = new Measurement()
+      {
+        MeasurementID = mesId
+      };
+      using (MySwoleMateEntities db = new MySwoleMateEntities())
+      {
+        db.Entry(deleteMes).State = System.Data.Entity.EntityState.Deleted;
+        db.SaveChanges();
+      }
+    }
+  }
+}
--- MySwoleMate.DAL/ScheduleDAL.cs
+++ MySwoleMate.DAL/ScheduleDAL.cs
@@ -3,16 +3,74 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MySwoleMate;
+using MySwoleMate.Models;
+
 
 namespace MySwoleMate.DAL
 {
   public class ScheduleDAL
   {
-    public void getSchedule()
+    //Returns All Schedules
+    public List<Schedule> GetSchedules()
     {
-      // Since it is using a database from outside of the Entity FrameWork we should use "Using" 
-      using (
+      List<Schedule> allSchedules = new List<Schedule>();
+      using (MySwoleMateEntities db = new MySwoleMateEntities())
+      {
+        allSchedules = db.Schedules.ToList();
+      }
+      return allSchedules;
+    }
+
+    //Get Schedule By Id
+    public Schedule GetScheduleById(int scId)
+    {
+      Schedule sc = new Schedule();
+      using (MySwoleMateEntities db = new MySwoleMateEntities())
+      {
+        sc = db.Schedules.Find();
+      }
+      return sc;
+    }
+
+    //Edit Schedule
+    public int EditSchedule(Schedule editSc)
+    {
+      using (MySwoleMateEntities db = new MySwoleMateEntities())
+      {
+        var sc = new Schedule { ScheduleID = editSc.ScheduleID, ScheduleDtTime = editSc.ScheduleDtTime, InputDtTime = editSc.ScheduleDtTime};
+        db.Entry(sc).State = System.Data.Entity.EntityState.Modified;
+        return db.SaveChanges();
+      }
+    }
+
+    //Add a Schedule
+    public int AddSchedule(Schedule newSc)
+    {
+      Schedule sc = new Schedule()
+      {
+        ScheduleDtTime = newSc.ScheduleDtTime,
+        InputDtTime = newSc.InputDtTime,
+      };
+
+      using (MySwoleMateEntities db = new MySwoleMateEntities())
+      {
+        db.Schedules.Add(sc);
+        return db.SaveChanges();
+      }
+    }
+
+    //Delete a Schedule
+    public void DeleteSchedule(int scId)
+    {
+      Schedule deleteSc = new Schedule()
+      {
+        ScheduleID = scId
+      };
+      using (MySwoleMateEntities db = new MySwoleMateEntities())
+      {
+        db.Entry(deleteSc).State = System.Data.Entity.EntityState.Deleted;
+        db.SaveChanges();
+      }
     }
   }
 }
