a4861cef551e0562e8705decf41ce874df0b6883
azimibm <sambm2512@gmail.com>
2021-02-08 23:12:30 -0600

    Added ScheduleBLL and MeasurementBLL files to the project

---
+++ MySwoleMate.BLL/ScheduleBLL.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySwoleMate.Models;
+using MySwoleMate.DAL;
+
+namespace MySwoleMate.BLL
+{
+  //The ScheduleBLL class will pass data through the Data Access Layer to the Web Forms application.
+  public class ScheduleBLL
+  {
+    private ScheduleDAL data;
+    public ScheduleBLL()
+    {
+      data = new ScheduleDAL();
+    }
+
+    public List<Schedule> GetSchedules()
+    {
+      List<Schedule> schedules = data.GetSchedules();
+      return schedules;
+    }
+
+    public int EditSchedule(Schedule edit)
+    {
+      return data.EditSchedule(edit);
+    }
+
+    public int AddSchedule(Schedule add)
+    {
+      return data.AddSchedule(add);
+    }
+
+    public void DeleteSchedule(int scID)
+    {
+      data.DeleteSchedule(scID);
+    }
+
+    public Schedule GetScheduleById(int scID)
+    {
+      return data.GetScheduleById(scID);
+    }
+  }
+}
